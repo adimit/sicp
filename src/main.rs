a2357e7d06f2e12b115364e6c6d777794f154c81
Aleksandar Dimitrov <git@aleks.bg>
2020-07-30 21:31:29 +0200

    rustfmt

--- src/main.rs
+++ src/main.rs
@@ -13,14 +13,17 @@
 
 use std::fmt;
 use std::io;
-use std::{error::Error, io::{stdout, Write}};
+use std::{
+    error::Error,
+    io::{stdout, Write},
+};
 
 #[derive(Debug)]
 enum ReplError {
     IOError(io::Error),
     TokenisationError(String),
     ParsingError(String),
-    EvaluationError(String)
+    EvaluationError(String),
 }
 
 impl Error for ReplError {}
@@ -28,10 +31,10 @@ impl Error for ReplError {}
 impl fmt::Display for ReplError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
-            ReplError::IOError(e) => { write!(f, "REPL IO error: {}", e)? }
-            ReplError::TokenisationError(msg) => { write!(f, "REPL tokenisation error: {}", msg)? }
-            ReplError::ParsingError(msg) => { write!(f, "RERPL parsing error: {}", msg)? }
-            ReplError::EvaluationError(msg) => {write!(f, "REPL evaluation error: {}", msg)? }
+            ReplError::IOError(e) => write!(f, "REPL IO error: {}", e)?,
+            ReplError::TokenisationError(msg) => write!(f, "REPL tokenisation error: {}", msg)?,
+            ReplError::ParsingError(msg) => write!(f, "RERPL parsing error: {}", msg)?,
+            ReplError::EvaluationError(msg) => write!(f, "REPL evaluation error: {}", msg)?,
         }
         Ok(())
     }
@@ -45,7 +48,7 @@ impl From<io::Error> for ReplError {
 
 enum Token {
     Number(i64),
-    Symbol(String)
+    Symbol(String),
 }
 
 enum Input {
@@ -92,17 +95,27 @@ fn read() -> Result<Input, ReplError> {
     let mut input = String::new();
     let bytes_read = io::stdin().read_line(&mut input)?;
 
-    if bytes_read == 0 {return Ok(Input::EOF)};
+    if bytes_read == 0 {
+        return Ok(Input::EOF);
+    };
     let tokens = tokenise(input.strip_suffix("\n").unwrap_or(&input))?;
 
     Ok(Input::Tokens(tokens))
 }
 
 fn evaluate_tokens(tokens: Vec<Token>) -> Result<EvaluationResult, ReplError> {
-    tokens.get(0).map(|token| { match token {
-        Token::Number(num) => { EvaluationResult::Expression(Expression::Int(*num))}
-        Token::Symbol(sym) => { EvaluationResult::Expression(Expression::String(sym.clone()))}
-    }}).map_or(Err(ReplError::EvaluationError(String::from("No tokens to evaluate"))), |result| { Ok(result) } )
+    tokens
+        .get(0)
+        .map(|token| match token {
+            Token::Number(num) => EvaluationResult::Expression(Expression::Int(*num)),
+            Token::Symbol(sym) => EvaluationResult::Expression(Expression::String(sym.clone())),
+        })
+        .map_or(
+            Err(ReplError::EvaluationError(String::from(
+                "No tokens to evaluate",
+            ))),
+            |result| Ok(result),
+        )
 }
 
 fn eval(input: Input) -> Result<EvaluationResult, ReplError> {
@@ -122,21 +135,17 @@ fn main() {
     loop {
         let output = repl();
         match output {
-            Ok(result) => {
-                match result {
-                    EvaluationResult::Expression(e) => {
-                        println!("{}", e);
-                    }
-                    EvaluationResult::Command(c) => {
-                        match c {
-                            Command::Quit => {
-                                println!("\nBye!");
-                                break;
-                            }
-                        }
-                    }
+            Ok(result) => match result {
+                EvaluationResult::Expression(e) => {
+                    println!("{}", e);
                 }
-            }
+                EvaluationResult::Command(c) => match c {
+                    Command::Quit => {
+                        println!("\nBye!");
+                        break;
+                    }
+                },
+            },
             Err(e) => {
                 println!("Encountered an error:\n{}", e);
             }
