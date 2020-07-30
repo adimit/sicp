14e3c58d683def682caf2857900cb734b22e17d2
Aleksandar Dimitrov <git@aleks.bg>
2020-07-30 14:21:53 +0200

    Add REPL

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,44 @@
+use std::io;
+use std::io::{stdout, Write};
+use std::fmt;
+
+enum Expression {
+    StringExpr(String)
+}
+
+impl fmt::Display for Expression {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Expression::StringExpr(s) => { write!(f, "{}", s)?; }
+        }
+        Ok(())
+    }
+}
+
+fn read() -> String {
+    print!("λ> ");
+    stdout().flush().expect("Failed to flush stdout.");
+
+    let mut input = String::new();
+
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read a line of input.");
+
+    match input.strip_suffix("\n") {
+        Some(s) => {String::from(s)}
+        None => {input}
+    }
+}
+
+fn eval(input: String) -> Expression {
+    Expression::StringExpr(input)
+}
+
 fn main() {
-    println!("Hello, world!");
+    loop {
+        let input = read();
+        let output = eval(input);
+        println!("{}", output);
+    }
 }
