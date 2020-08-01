e5ea5dccb8f47f76a042e769f689add298271cec
Aleksandar Dimitrov <git@aleks.bg>
2020-08-01 12:03:00 +0200

    Add spans to tokenisation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+#![feature(peekable_next_if)]
+
 use std::fmt;
 use std::io;
 use std::{
@@ -34,7 +36,28 @@ impl From<io::Error> for ReplError {
 }
 
 #[derive(Debug, Eq, PartialEq)]
-enum Token {
+struct Span {
+    begin: usize,
+    end: usize,
+}
+
+#[derive(Debug, Eq, PartialEq)]
+struct Token {
+    content: TokenData,
+    span: Span,
+}
+
+impl Token {
+    fn create(begin: usize, end: usize, content: TokenData) -> Self {
+        Token {
+            span: Span { begin, end },
+            content,
+        }
+    }
+}
+
+#[derive(Debug, Eq, PartialEq)]
+enum TokenData {
     OpenParen,
     ClosedParen,
     Number(i64),
@@ -77,94 +100,65 @@ impl fmt::Display for Expression {
     }
 }
 
-#[must_use = "iterators are lazy and do nothing unless consumed"]
-#[derive(Clone)]
-struct TakeWhilePeeking<I, P> {
-    iterator: I,
-    empty: bool,
-    predicate: P
-}
-
-impl<I: Iterator, P> Iterator for TakeWhilePeeking<&mut std::iter::Peekable<I>, P> where P: FnMut(&I::Item) -> bool {
-    type Item = I::Item;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        if self.empty {
-            None
-        } else {
-            let peeked = self.iterator.peek()?;
-            if (self.predicate)(peeked) {
-                self.iterator.next()
-            } else {
-                self.empty = true;
-                None
-            }
-        }
-    }
-}
-
-fn take_while_peeking<I: std::iter::Iterator, P: FnMut(&I::Item) -> bool>(
-    i: &mut std::iter::Peekable<I>, p: P
-) -> TakeWhilePeeking<&mut std::iter::Peekable<I>, P> {
-    TakeWhilePeeking {
-        iterator: i,
-        empty: false,
-        predicate: p
-    }
-}
-
-fn parse_digits(chars:&mut std::iter::Peekable<std::str::Chars>) -> Result<i64, ReplError> {
-    let string: String = take_while_peeking(chars, |c| { c.is_digit(10) }).collect();
-    string.parse().map_err(|err| { ReplError::TokenisationError(format!("Error parsing string: {}", err)) })
-}
-
-fn parse_symbol(chars:&mut std::iter::Peekable<std::str::Chars>) -> Result<String, ReplError> {
-    Ok(take_while_peeking(chars, |c| { !c.is_whitespace() }).collect())
-}
-
-fn expect_one_of(chars: &mut std::iter::Peekable<std::str::Chars>, next: Vec<char>) -> Result<(), ReplError> {
-    match chars.peek() {
-        Some(c) if next.contains(c) => Ok(()),
-        Some(c) => Err(ReplError::TokenisationError(format!("Unexpected {}. Expected one of {}.", c, render_alternatives(next)))),
-        None => Err(ReplError::TokenisationError(format!("Unexpected end of string. Expected one of {}", render_alternatives(next))))
-    }
-}
-
-fn expect_end_of_string(chars: &mut std::iter::Peekable<std::str::Chars>) -> Result<(), ReplError> {
-    match chars.peek() {
-        Some(c) => Err(ReplError::TokenisationError(format!("Unexpected {}. Expected end of string.", c))),
-        None => Ok(())
-    }
-}
-
-fn render_alternatives(v: Vec<char>) -> String {
-    v.iter().map(|c| { format!("`{}`", c) }).collect::<Vec<String>>().join(", ")
-}
+type Tape<'a> = std::iter::Peekable<std::iter::Enumerate<std::str::Chars<'a>>>;
 
 fn tokenise(string: &str) -> Result<Vec<Token>, ReplError> {
-    let mut chars = string.chars().peekable();
-    let mut tokens: Vec<Token> = Vec::new();
-
-    loop {
-        match chars.peek() {
-            Some('(') => { chars.next(); tokens.push(Token::OpenParen) },
-            Some(')') => { chars.next(); tokens.push(Token::ClosedParen) },
-            Some(x) if x.is_whitespace() => { chars.next(); },
-            Some(x) if x.is_digit(10) => {
-                let number = parse_digits(&mut chars)?;
-                tokens.push(Token::Number(number));
-                expect_one_of(&mut chars, vec![' ', '(', ')'])
-                    .or(expect_end_of_string(&mut chars))?
-            },
-            Some(_) => {
-                let symbol = parse_symbol(&mut chars)?;
-                tokens.push(Token::Symbol(symbol));
-            },
-            None => { break; }
-        }
+    let mut chars = string.chars().enumerate().peekable();
+    let mut tokens = Vec::new();
+    while let Some(token) = parse_a_token(&mut chars) {
+        tokens.push(token);
     }
 
-    Ok(tokens)
+    match chars.peek() {
+        Some((pos, c)) => Err(ReplError::TokenisationError(format!(
+            "Unrecognised input `{}` at position {}.",
+            c, pos
+        ))),
+        None => Ok(tokens),
+    }
+}
+
+fn parse_a_token(tape: &mut Tape) -> Option<Token> {
+    parse_whitespace(tape);
+    let r = parse_open_paren(tape)
+        .or_else(|| parse_closed_paren(tape))
+        .or_else(|| parse_symbol(tape));
+    parse_whitespace(tape);
+    r
+}
+
+fn parse_open_paren(tape: &mut Tape) -> Option<Token> {
+    tape.next_if(|(_, c)| *c == '(')
+        .map(|(pos, _)| Token::create(pos, pos, TokenData::OpenParen))
+}
+
+fn parse_closed_paren(tape: &mut Tape) -> Option<Token> {
+    tape.next_if(|(_, c)| *c == ')')
+        .map(|(pos, _)| Token::create(pos, pos, TokenData::ClosedParen))
+}
+
+fn parse_whitespace(tape: &mut Tape) -> Option<()> {
+    tape.next_if(|(_, c)| c.is_whitespace()).map(|(_, _)| ())
+}
+
+fn parse_symbol(tape: &mut Tape) -> Option<Token> {
+    let mut string = String::new();
+    let mut lastpos = 0;
+    while let Some((pos, c)) = tape.next_if(|(_, c)| !(c.is_whitespace() || *c == '(' || *c == ')'))
+    {
+        string.push(c);
+        lastpos = pos;
+    }
+
+    if string.len() > 0 {
+        let start: usize = lastpos - (string.len() - 1);
+        match string.parse::<i64>() {
+            Ok(num) => Some(Token::create(start, lastpos, TokenData::Number(num))),
+            Err(_) => Some(Token::create(start, lastpos, TokenData::Symbol(string))),
+        }
+    } else {
+        None
+    }
 }
 
 fn read() -> Result<Input, ReplError> {
@@ -186,11 +180,15 @@ fn read() -> Result<Input, ReplError> {
 fn evaluate_tokens(tokens: Vec<Token>) -> Result<EvaluationResult, ReplError> {
     tokens
         .get(0)
-        .map(|token| match token {
-            Token::Number(num) => EvaluationResult::Expression(Expression::Int(*num)),
-            Token::Symbol(sym) => EvaluationResult::Expression(Expression::String(sym.clone())),
-            Token::OpenParen => { EvaluationResult::Expression(Expression::String(String::from("(")))}
-            Token::ClosedParen => { EvaluationResult::Expression(Expression::String(String::from(")")))}
+        .map(|token| match &token.content {
+            TokenData::Number(num) => EvaluationResult::Expression(Expression::Int(*num)),
+            TokenData::Symbol(sym) => EvaluationResult::Expression(Expression::String(sym.clone())),
+            TokenData::OpenParen => {
+                EvaluationResult::Expression(Expression::String(String::from("(")))
+            }
+            TokenData::ClosedParen => {
+                EvaluationResult::Expression(Expression::String(String::from(")")))
+            }
         })
         .map_or(
             Err(ReplError::EvaluationError(String::from(
@@ -243,28 +241,6 @@ mod tests {
     use super::*;
 
     #[test]
-    fn take_while_peeking_leaves_last_element_intact() {
-        let mut iter = [1,2,3].iter().cloned().peekable();
-        let f: Vec<u8> = take_while_peeking(&mut iter, |n| { *n < 3}).collect();
-        assert_eq!(f, vec!(1,2));
-        assert_eq!(iter.next(), Some(3));
-    }
-
-    #[test]
-    fn parse_digits_parses_number() {
-        let mut chars = "123".chars().peekable();
-        assert_eq!(parse_digits(&mut chars).unwrap(), 123)
-    }
-
-    #[test]
-    fn expect_one_of_passes_on_correct_character() {
-        let mut iter = "ab".chars().peekable();
-        assert!(expect_one_of(&mut iter, vec!('b', 'c')).is_err());
-        iter.next();
-        assert!(expect_one_of(&mut iter, vec!('b', 'c')).is_ok());
-    }
-
-    #[test]
     fn evaluates_primitive_expressions() {
         assert_eq!(
             eval(Input::Line(String::from("123"))).unwrap(),
@@ -275,7 +251,31 @@ mod tests {
     #[test]
     fn tokenises_expressions() {
         let result = tokenise("(foo bar 123)").unwrap();
-        assert_eq!(result, [Token::OpenParen, Token::Symbol(String::from("foo")), Token::Symbol(String::from("bar")), Token::Number(123), Token::ClosedParen]);
+        assert_eq!(
+            result,
+            [
+                Token {
+                    span: Span { begin: 0, end: 0 },
+                    content: TokenData::OpenParen
+                },
+                Token {
+                    span: Span { begin: 1, end: 3 },
+                    content: TokenData::Symbol("foo".to_string())
+                },
+                Token {
+                    span: Span { begin: 5, end: 7 },
+                    content: TokenData::Symbol("bar".to_string())
+                },
+                Token {
+                    span: Span { begin: 9, end: 11 },
+                    content: TokenData::Number(123)
+                },
+                Token {
+                    span: Span { begin: 12, end: 12 },
+                    content: TokenData::ClosedParen
+                },
+            ]
+        )
     }
 
     #[test]
